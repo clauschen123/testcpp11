e3da06ca9d451389af79ff47565c0a18f6dffde4
ctj <ctj@kuka.com>
2015-11-28 10:02:53 +0800

    .

--- test_c11/main.cpp
+++ test_c11/main.cpp
@@ -49,30 +49,81 @@ UINT32 Increase(int addval, int byteoffset)
     return totalval;
 }
 
+///////////////////////////////////////////
+enum EFindLowHP  {};
+struct demo{
+    vector<int> vec=vector<int>(1, 100);
+
+    int val = 0;
+    bool isman = true;
+    demo()
+    {}
+    demo(int _val, bool _isman = true) : val(_val), isman(_isman)
+    {}
+    bool isMan() const { return isman; }
+    bool isDog() const { return !isman; }
+    int getVal() const { return val; }
+    bool operator() (const demo& d, EFindLowHP)
+    {
+        if (isMan() && !d.isMan())
+            return true;
+        if (!isMan() && d.isMan())
+            return false;
+        return getVal() > d.getVal();
+    }
+};
+///////////////////////////////////////////////////////
+
+void printS(string&& str)
+{
+    string a = std::move(str);
+    cout << str;
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
+    printS("sldfjlsfd)");
+    array<int, 10> arr;
+    demo d;
+    do 
+    {
+        {
+            int i = 1;
+            vector<demo> v;
+            v.emplace_back(i++, false);
+            v.emplace_back(i++);
+            v.emplace_back(i++,false);
+            v.emplace_back(i++);
+            v.emplace_back(i++, false);
+            sort(v.begin(), v.end(), [](demo & left, demo & right) {
+                return left(right, EFindLowHP(0));
+            });
+            for_each(v.begin(), v.end(), [](demo const& e){ cout << e.getVal() << "," << e.isMan() << endl; });
+
+            break;
+        }
     {
         vector<int> c1{ 1, 2, 3, 4 };
-        vector<int> c2{ 4,5, 7};
+        vector<int> c2{ 4, 5, 7 };
         set_difference(c2.cbegin(), c2.cend(),
             c1.cbegin(), c1.cend(),
             ostream_iterator<int>(cout, " "));
     }
     {
-        vector<int> uidlist_{2,3,2,3};
+        vector<int> uidlist_{ 2, 3, 2, 3 };
         int uid = 3;
 
-        uidlist_.erase(remove(uidlist_.begin(), uidlist_.end(), uid),uidlist_.end());
+        uidlist_.erase(remove(uidlist_.begin(), uidlist_.end(), uid), uidlist_.end());
     }
 
-    int zone[] = { 3 };
-    int uid[] = { 2532 };
-    unsigned long long rid = 0;
-    for (int i = 0; i < 1; i++){
-        rid = (((unsigned long long)zone[i]) << 52);
-        rid += uid[i];
-        cout << rid << endl;
-    }
+        int zone[] = { 3 };
+        int uid[] = { 2532 };
+        unsigned long long rid = 0;
+        for (int i = 0; i < 1; i++){
+            rid = (((unsigned long long)zone[i]) << 52);
+            rid += uid[i];
+            cout << rid << endl;
+        }
+    } while (0);
 
 	::system("pause");
 	return 0;
