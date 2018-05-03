57cc84c6f545bbf6d836b91944507789bd429544
clauschen <7301354@qq.com>
2018-05-03 14:46:44 +0800

    deadline timer test

--- test_c11/test_asio.cpp
+++ test_c11/test_asio.cpp
@@ -132,7 +132,9 @@ namespace asio
 #include <istream>
 #include <iostream>
 #include <ostream>
-
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include "icmp_header.hpp"
 #include "ipv4_header.hpp"
 
@@ -144,9 +146,9 @@ namespace ping {
     class pinger
     {
     public:
-        pinger(boost::asio::io_service& io_service, const char* destination)
-            : resolver_(io_service), socket_(io_service, icmp::v4()),
-            timer_(io_service), sequence_number_(0), num_replies_(0)
+        pinger(boost::asio::io_service& io_service, const char* destination, int ping_count=2)
+            : io_service_(io_service), resolver_(io_service), socket_(io_service, icmp::v4())
+            , timer_(io_service), sequence_number_(0), num_replies_(0), ping_count_(ping_count)
         {
             icmp::resolver::query query(icmp::v4(), destination, "");
             destination_ = *resolver_.resolve(query);
@@ -176,25 +178,43 @@ namespace ping {
             // Send the request.
             time_sent_ = posix_time::microsec_clock::universal_time();
             socket_.send_to(request_buffer.data(), destination_);
+            std::cout <<">>>> Send at " << (time_sent_) << std::endl;
 
             // Wait up to five seconds for a reply.
-            num_replies_ = 0;
+            //num_replies_ = 0;
             timer_.expires_at(time_sent_ + posix_time::seconds(5));
-            timer_.async_wait(boost::bind(&pinger::handle_timeout, this));
+            timer_.async_wait(boost::bind(&pinger::handle_timeout, this, boost::asio::placeholders::error));
         }
 
-        void handle_timeout()
+        void handle_timeout( const boost::system::error_code& err)
         {
-            if (num_replies_ == 0)
-                std::cout << "Request timed out" << std::endl;
+            if (err) {
+                auto now = posix_time::microsec_clock::universal_time();
+                std::cout << "Request timed out, replies=" << num_replies_ << ",at time:" << now << std::endl;
+            }
+            else {
+                std::cout << "Request timed out, not err " << std::endl;
+            }
 
-            // Requests must be sent no less than one second apart.
-            timer_.expires_at(time_sent_ + posix_time::seconds(1));
-            timer_.async_wait(boost::bind(&pinger::start_send, this));
+            //if (num_replies_ == 0)
+
+            if (false) {
+                timer_.cancel();
+                io_service_.stop();
+                return;
+            }
+
+            if (!should_end()) {
+                // Requests must be sent no less than one second apart.
+                timer_.expires_at(time_sent_ + posix_time::seconds(1));
+                timer_.async_wait(boost::bind(&pinger::start_send, this));
+            }
         }
 
         void start_receive()
         {
+            std::cout << "----------Start receive " << std::endl;
+
             // Discard any data already in the buffer.
             reply_buffer_.consume(reply_buffer_.size());
 
@@ -223,8 +243,12 @@ namespace ping {
                 && icmp_hdr.sequence_number() == sequence_number_)
             {
                 // If this is the first reply, interrupt the five second timeout.
-                if (num_replies_++ == 0)
+                if (num_replies_++ == 0) {
+                    std::cout << " cancel timer replyes:"<< num_replies_ 
+                        <<",now:" << posix_time::microsec_clock::universal_time() 
+                        << std::endl;
                     timer_.cancel();
+                }
 
                 // Print out some information about the reply packet.
                 posix_time::ptime now = posix_time::microsec_clock::universal_time();
@@ -233,10 +257,28 @@ namespace ping {
                     << ": icmp_seq=" << icmp_hdr.sequence_number()
                     << ", ttl=" << ipv4_hdr.time_to_live()
                     << ", time=" << (now - time_sent_).total_milliseconds() << " ms"
+                    << ", replies=" << num_replies_ 
                     << std::endl;
-            }
 
+                int64_t this_intval = (now - time_sent_).total_milliseconds();
+                if (ping_intval_ == 0) {
+                    ping_intval_ = this_intval;
+                }
+                else {
+                    ping_intval_ = (ping_intval_ + this_intval) / 2;
+                }
+            }
+            if (should_end()) {
+                io_service_.stop();
+                return;
+            }
             start_receive();
+        }
+
+        bool should_end() 
+        {
+            //std::cout << "replyes:" << num_replies_ << ", ping count:" << ping_count_ << std::endl;
+            return num_replies_ >= ping_count_;
         }
 
         static unsigned short get_identifier()
@@ -248,6 +290,7 @@ namespace ping {
 #endif
         }
 
+        boost::asio::io_service& io_service_;
         icmp::resolver resolver_;
         icmp::endpoint destination_;
         icmp::socket socket_;
@@ -256,28 +299,52 @@ namespace ping {
         posix_time::ptime time_sent_;
         boost::asio::streambuf reply_buffer_;
         std::size_t num_replies_;
+        int ping_count_ = 0;
+        time_t ping_intval_ = 0;
     };
 
     int main() {
         try
         {
-//             if (argc != 2)
-//             {
-//                 std::cerr << "Usage: ping <host>" << std::endl;
-// #if !defined(BOOST_WINDOWS)
-//                 std::cerr << "(You may need to run this program as root.)" << std::endl;
-// #endif
-//                 return 1;
-//             }
-
             boost::asio::io_service io_service;
-            pinger p(io_service, "www.baidu.com");
+            pinger p(io_service, "127.0.0.1");
             io_service.run();
+
+            std::cout << " ios run exit!" << std::endl;
         }
         catch (std::exception& e)
         {
             std::cerr << "Exception: " << e.what() << std::endl;
         }
+        return 0;
+    }
+
+    int main1()
+    {
+        boost::gregorian::date dayte(boost::gregorian::day_clock::local_day());
+        boost::posix_time::ptime midnight(dayte);
+        boost::posix_time::ptime now(boost::posix_time::microsec_clock::local_time());
+        boost::posix_time::time_duration td = now - midnight;
+
+        std::stringstream sstream;
+
+        std::cout << dayte << std::endl;
+        std::cout << dayte.year() << "/" << dayte.month().as_number()
+            << "/" << dayte.day() << std::endl;
+        std::cout << now << std::endl;
+        std::cout << td << std::endl;
+        std::cout << td.hours() << "/" << td.minutes() << "/"
+            << td.seconds() << "/" << td.fractional_seconds() << std::endl;
+
+        sstream << dayte << std::endl;
+        sstream << dayte.year() << "/" << dayte.month().as_number()
+            << "/" << dayte.day() << std::endl;
+        sstream << now << std::endl;
+        sstream << td << std::endl;
+        sstream << td.hours() << "/" << td.minutes() << "/" << td.seconds()
+            << "/" << td.fractional_seconds() << std::endl;
+
+        std::cout << sstream.str();
         return 0;
     }
 }
